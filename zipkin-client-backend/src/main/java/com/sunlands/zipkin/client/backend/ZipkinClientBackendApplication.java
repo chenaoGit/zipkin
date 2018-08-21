43f3a6dd0b7cba40719b2542553225880a770a6e
chenao <chenao@sunlands.com>
2018-08-21 19:40:35 +0800

    Initial commit

---
+++ zipkin-client-backend/src/main/java/com/sunlands/zipkin/client/backend/ZipkinClientBackendApplication.java
@@ -0,0 +1,12 @@
+package com.sunlands.zipkin.client.backend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ZipkinClientBackendApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ZipkinClientBackendApplication.class, args);
+    }
+}
---
+++ zipkin-client-backend/src/main/java/com/sunlands/zipkin/client/backend/service/UserService.java
@@ -0,0 +1,16 @@
+//package com.sunlands.zipkin.client.backend.service;
+//
+//import org.springframework.stereotype.Service;
+//
+///**
+// * @Author chenao
+// * @Description
+// * @Date Created in 2018/8/13.
+// */
+//@Service
+//public class UserService {
+//
+//    public boolean isExist(String id) {
+//        return false;
+//    }
+//}
